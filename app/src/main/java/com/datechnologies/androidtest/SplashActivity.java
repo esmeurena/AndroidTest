9b73d333a8b5e01b2bda68862a3b1aa675a55b86
esmeurena <33582708+esmeurena@users.noreply.github.com>
2022-01-08 13:39:05 -0800

    Splash Screen completed

--- app/src/main/java/com/datechnologies/androidtest/SplashActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/SplashActivity.java
@@ -1,6 +1,20 @@
 package com.datechnologies.androidtest;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+
 }
