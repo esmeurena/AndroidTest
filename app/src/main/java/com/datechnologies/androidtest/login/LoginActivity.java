be84be9bf1f3ad59c1fecdd33c9cd0956542c045
esmeurena <33582708+esmeurena@users.noreply.github.com>
2022-01-10 11:11:40 -0800

    Login Screen completed

--- app/src/main/java/com/datechnologies/androidtest/login/LoginActivity.java
+++ app/src/main/java/com/datechnologies/androidtest/login/LoginActivity.java
@@ -63,5 +63,6 @@ public class LoginActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 }
